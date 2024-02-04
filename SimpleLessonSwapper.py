6b2028d69608d20dd42461beb41c54bb8cc63f84
Jovita Tang <47584623+Brennil@users.noreply.github.com>
2024-02-04 10:22:21 +0800

    Update SimpleLessonSwapper.py

--- SimpleLessonSwapper.py
+++ SimpleLessonSwapper.py
@@ -208,7 +208,7 @@ if st.button("Click me to see who is free!"):
                 exp += 1
     for teach in teachers_list:
         x = availableper(teach)
-        print(x[day.split()[1]])
+        st.write(x)
         if lesson_period in x[day.split()[1]]:
             if class_toswap in teacherdb[teach]:
                 teachers_class_free.append(teach)
